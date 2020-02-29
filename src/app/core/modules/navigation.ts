7706a6d8d30cdd76ccf70a9bcdb135cdd5949087
pcboby <pcboby@126.com>
2020-02-29 13:42:45 +0800

    update

--- src/app/core/modules/navigation.ts
+++ src/app/core/modules/navigation.ts
@@ -2,35 +2,35 @@ export const Navigation = [
   {
     title: '图层控制',
     link: '/map/layer',
-    iconCls: 'fa fa-cog',
+    iconCls: 'icon-menu-layer',
     promise: '',
     data: [
       {
         title: '矢量数据',
         link: '/map/layer',
         queryParams: {type: '矢量数据'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-layer-vector',
         promise: ''
       },
       {
         title: '模型数据',
         link: '/map/layer',
         queryParams: {type: '模型数据'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-layer-module',
         promise: ''
       },
       {
         title: '管线数据',
         link: '/map/layer',
         queryParams: {type: '管线数据'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-layer-pipeline',
         promise: ''
       },
       {
         title: '影像数据',
         link: '/map/layer',
         queryParams: {type: '影像数据'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-layer-image',
         promise: ''
       }
     ]
@@ -38,28 +38,28 @@ export const Navigation = [
   {
     title: '地图操作',
     link: '/map/control',
-    iconCls: 'fa fa-cog',
+    iconCls: 'icon-menu-control',
     promise: '',
     data: [
       {
         title: '测量工具',
         link: '/map/control',
         queryParams: {type: '测量工具'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-control-measure',
         promise: ''
       },
       {
         title: '标注工具',
         link: '/map/control',
         queryParams: {type: '标注工具'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-control-tagging',
         promise: ''
       },
       {
         title: '定位工具',
         link: '/map/control',
         queryParams: {type: '定位工具'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-control-location',
         promise: ''
       }
     ]
@@ -67,63 +67,63 @@ export const Navigation = [
   {
     title: '管线查询',
     link: '/map/pipeline',
-    iconCls: 'fa fa-cog',
+    iconCls: 'icon-menu-pipeline',
     promise: '',
     data: [
       {
         title: '点击查询',
         link: '/map/pipeline',
         queryParams: {type: '点击查询'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-pipeline-click',
         promise: ''
       },
       {
         title: '管径',
         link: '/map/pipeline',
         queryParams: {type: '管径'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-pipeline-size',
         promise: ''
       },
       {
         title: '材质',
         link: '/map/pipeline',
         queryParams: {type: '材质'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-pipeline-material',
         promise: ''
       },
       {
         title: '附属物',
         link: '/map/pipeline',
         queryParams: {type: '附属物'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-pipeline-appendages',
         promise: ''
       },
       {
         title: '复合查询',
         link: '/map/pipeline',
         queryParams: {type: '复合查询'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-pipeline-combination',
         promise: ''
       },
       {
         title: '管点使用状态录入',
         link: '/map/pipeline',
         queryParams: {type: '管点使用状态录入'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-pipeline-point',
         promise: ''
       },
       {
         title: '能源系统图录入',
         link: '/map/pipeline',
         queryParams: {type: '能源系统图录入'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-pipeline-power',
         promise: ''
       },
       {
         title: '巡视管理',
         link: '/map/pipeline',
         queryParams: {type: '巡视管理'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-pipeline-patrol',
         promise: ''
       }
     ]
@@ -131,7 +131,7 @@ export const Navigation = [
   {
     title: '管线统计',
     link: '/map/statistics',
-    iconCls: 'fa fa-cog',
+    iconCls: 'icon-menu-statistics',
     promise: '',
     data: [
       {
@@ -181,56 +181,56 @@ export const Navigation = [
   {
     title: '综合分析',
     link: '/map/analysis',
-    iconCls: 'fa fa-cog',
+    iconCls: 'icon-menu-analysis',
     promise: '',
     data: [
       {
         title: '横断面分析',
         link: '/map/analysis',
         queryParams: {type: '横断面分析'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-analysis-cross',
         promise: ''
       },
       {
         title: '爆管分析',
         link: '/map/analysis',
         queryParams: {type: '爆管分析'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-analysis-tube',
         promise: ''
       },
       {
         title: '连通分析',
         link: '/map/analysis',
         queryParams: {type: '连通分析'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-analysis-connected',
         promise: ''
       },
       {
         title: '挖方分析',
         link: '/map/analysis',
         queryParams: {type: '挖方分析'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-analysis-excavation',
         promise: ''
       },
       {
         title: '水平净距',
         link: '/map/analysis',
         queryParams: {type: '水平净距'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-analysis-horizontal',
         promise: ''
       },
       {
         title: '垂直净距',
         link: '/map/analysis',
         queryParams: {type: '垂直净距'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-analysis-vertical',
         promise: ''
       },
       {
         title: '健康分析',
         link: '/map/analysis',
         queryParams: {type: '健康分析'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-analysis-healthy',
         promise: ''
       }
     ]
@@ -238,28 +238,28 @@ export const Navigation = [
   {
     title: '房属管理',
     link: '/map/house',
-    iconCls: 'fa fa-cog',
+    iconCls: 'icon-menu-house',
     promise: '',
     data: [
       {
         title: '属性挂接',
         link: '/map/house',
         queryParams: {type: '属性挂接'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-house-attrib',
         promise: ''
       },
       {
         title: '信息查询',
         link: '/map/house',
         queryParams: {type: '信息查询'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-house-information',
         promise: ''
       },
       {
         title: '信息修改',
         link: '/map/house',
         queryParams: {type: '信息修改'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-house-modify',
         promise: ''
       }
     ]
@@ -267,21 +267,21 @@ export const Navigation = [
   {
     title: '能源系统',
     link: '/map/energy',
-    iconCls: 'fa fa-cog',
+    iconCls: 'icon-menu-energy',
     promise: '',
     data: [
       {
         title: '监测计量',
         link: '/map/energy',
         queryParams: {type: '监测计量'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-energy-monitoring',
         promise: ''
       },
       {
         title: '能源系统图',
         link: '/map/energy',
         queryParams: {type: '能源系统图'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-energy-map',
         promise: ''
       }
     ]
@@ -289,14 +289,14 @@ export const Navigation = [
   {
     title: '打印出图',
     link: '/map/print',
-    iconCls: 'fa fa-cog',
+    iconCls: 'icon-menu-print',
     promise: '',
     data: [
       {
         title: '专题图输出',
         link: '/map/print',
         queryParams: {type: '专题图输出'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-print-special',
         promise: ''
       }
     ]
@@ -304,28 +304,28 @@ export const Navigation = [
   {
     title: '系统管理',
     link: '/system',
-    iconCls: 'fa fa-cog',
+    iconCls: 'icon-menu-system',
     promise: '',
     data: [
       {
         title: '机构管理',
         link: '/system',
         queryParams: {type: '机构管理'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-system-mechanism',
         promise: ''
       },
       {
         title: '权限管理',
         link: '/system',
         queryParams: {type: '权限管理'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-system-authority',
         promise: ''
       },
       {
         title: '日志管理',
         link: '/system',
         queryParams: {type: '日志管理'},
-        iconCls: 'fa fa-cog',
+        iconCls: 'icon-menu-system-log',
         promise: ''
       }
     ]
