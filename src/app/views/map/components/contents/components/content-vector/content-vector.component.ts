06c5e9be16d1ced69c0f05b06b870fea2cec121c
pcboby <pcboby@126.com>
2020-02-29 11:07:39 +0800

    update

--- src/app/views/map/components/contents/components/content-vector/content-vector.component.ts
+++ src/app/views/map/components/contents/components/content-vector/content-vector.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NzFormatEmitEvent } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-content-vector',
@@ -6,10 +7,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content-vector.component.css']
 })
 export class ContentVectorComponent implements OnInit {
+  defaultCheckedKeys = ['0-0-0'];
+  defaultSelectedKeys = ['0-0-0'];
+  defaultExpandedKeys = ['0-0', '0-0-0', '0-0-1'];
+
+  nodes = [
+    {
+      title: '0-0',
+      key: '0-0',
+      expanded: true,
+      children: [
+        {
+          title: '0-0-0',
+          key: '0-0-0',
+          children: [
+            { title: '0-0-0-0', key: '0-0-0-0', isLeaf: true },
+            { title: '0-0-0-1', key: '0-0-0-1', isLeaf: true },
+            { title: '0-0-0-2', key: '0-0-0-2', isLeaf: true }
+          ]
+        },
+        {
+          title: '0-0-1',
+          key: '0-0-1',
+          children: [
+            { title: '0-0-1-0', key: '0-0-1-0', isLeaf: true },
+            { title: '0-0-1-1', key: '0-0-1-1', isLeaf: true },
+            { title: '0-0-1-2', key: '0-0-1-2', isLeaf: true }
+          ]
+        },
+        {
+          title: '0-0-2',
+          key: '0-0-2',
+          isLeaf: true
+        }
+      ]
+    },
+    {
+      title: '0-1',
+      key: '0-1',
+      children: [
+        { title: '0-1-0-0', key: '0-1-0-0', isLeaf: true },
+        { title: '0-1-0-1', key: '0-1-0-1', isLeaf: true },
+        { title: '0-1-0-2', key: '0-1-0-2', isLeaf: true }
+      ]
+    },
+    {
+      title: '0-2',
+      key: '0-2',
+      isLeaf: true
+    }
+  ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+  nzEvent(event: NzFormatEmitEvent): void {
+    console.log(event);
+  }
+
+  afterClose(): void {
+    console.log('close');
   }
 
 }
